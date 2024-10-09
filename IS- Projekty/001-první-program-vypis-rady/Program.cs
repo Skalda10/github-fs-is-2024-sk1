103b6df8a42752a42e4931c52f38f337f0e325c5
Skalda10 <filip.skalicky@tul.cz>
2024-10-09 09:42:57 +0200

    #1 cýpis řady základ programu s opakováním

--- IS- Projekty/001-první-program-vypis-rady/Program.cs
+++ IS- Projekty/001-první-program-vypis-rady/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System
+
+class Program
+{
+  static void Main() {;
+    
+    string again = "a";
+   
+    while(again=="a") {
+            Console.Clear();
+            Console.WriteLine("*************************");
+            Console.WriteLine("********** Výpis řady čísel ***************");
+            Console.WriteLine("*************************");
+            Console.WriteLine("*********** FilipSkalický ***********");
+            Console.WriteLine();
+        
+     
+     
+            Console.WriteLine()
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();
+            
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+          }
+     
+
+    }
+}
+
+
+
+// toto je jedno řádkový komentář
+
+
+/* toto je
+víceřádkový komentář */
+
+
+
+
+
