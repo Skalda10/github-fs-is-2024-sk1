a6b6dd078219e04d7461b81c5dcd324d6a55620a
Skalda10 <filip.skalicky@tul.cz>
2024-10-16 09:21:37 +0200

    #1 Výpis řad - done

--- IS- Projekty/001-první-program-vypis-rady/001-App1/obj/Debug/net8.0/001-App1.AssemblyInfo.cs
+++ IS- Projekty/001-první-program-vypis-rady/001-App1/obj/Debug/net8.0/001-App1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("001-první-program-vypis-rady")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("001-App1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be369dc8d0f95dbee930dbbc812932077db41195")]
-[assembly: System.Reflection.AssemblyProductAttribute("001-první-program-vypis-rady")]
-[assembly: System.Reflection.AssemblyTitleAttribute("001-první-program-vypis-rady")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103b6df8a42752a42e4931c52f38f337f0e325c5")]
+[assembly: System.Reflection.AssemblyProductAttribute("001-App1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("001-App1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Vygenerované třídou WriteCodeFragment nástroje MSBuild
