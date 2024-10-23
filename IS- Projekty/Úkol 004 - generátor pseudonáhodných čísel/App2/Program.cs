df853d39050778e690237e9fa642a40722a675f3
Skalda10 <filip.skalicky@tul.cz>
2024-10-23 09:51:05 +0200

    #4 Generátor - základ

--- IS- Projekty/Úkol 004 - generátor pseudonáhodných čísel/App2/Program.cs
+++ IS- Projekty/Úkol 004 - generátor pseudonáhodných čísel/App2/Program.cs
@@ -5,9 +5,10 @@
             Console.WriteLine("*******************************************");
             Console.WriteLine("***** Generátor pseudonáhodných čísel *****");
             Console.WriteLine("*******************************************");
-            Console.WriteLine("************* Tomáš Žižka *****************");
+            Console.WriteLine("************* Filip Skalický **************");
             Console.WriteLine("*******************************************");
             Console.WriteLine();
+            
             Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
             int n;
             while(!int.TryParse(Console.ReadLine(), out n)) {
@@ -29,7 +30,20 @@
             Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez: {2}", n, dm, hm);
             Console.WriteLine("==========================================");
             Console.WriteLine();
+            //deklarace pole    
+            int[] myArray = new int[n];
+            Random randomNumber = new Random();
+            Console.WriteLine("\n\nNáhodná čísla:");
+            for(int i=0; i<n; i++) {
+                myArray[i] = randomNumber.Next(dm, hm+1);
+                Console.Write("{0}; ", myArray[i]); 
             
+            
+            
+            
+            
+            
+            }
             Console.WriteLine();
             Console.WriteLine("Pro opakování programu stiskněte klávesu A");
             again = Console.ReadLine();
