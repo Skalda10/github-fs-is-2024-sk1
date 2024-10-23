ab5b5062b1e1f522f133178704398e6abdeeda5d
Skalda10 <filip.skalicky@tul.cz>
2024-10-23 09:12:16 +0200

    #4 Prázdný projekt

--- IS- Projekty/README.md
+++
@@ -1,3 +0,0 @@
-### Informace k algoritmizaci s využitím jakyza C#
-
-Složka **IS-Projekty** slouží pro projekty v C#
--- IS- Projekty/Úkol 004 - generátor pseudonáhodných čísel/App2/obj/Debug/net8.0/App2.AssemblyInfo.cs
+++ IS- Projekty/Úkol 004 - generátor pseudonáhodných čísel/App2/obj/Debug/net8.0/App2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("App2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551d41cb103d98253c36f3beece5eff3941230c9")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyProductAttribute("App2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("App2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Vygenerované třídou WriteCodeFragment nástroje MSBuild
